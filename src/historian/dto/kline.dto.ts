3ab97674aa2176a8162d084d7de15d6d876af0e4
Volodymyr <vartallerrab@gmail.com>
2025-02-05 19:15:42 +0100

    done: historian formatting

---
+++ src/historian/dto/kline.dto.ts
@@ -0,0 +1,9 @@
+export class KlineOutputDto {
+  requestId: number;
+  date: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+}
--- src/historian/historian.service.ts
+++ src/historian/historian.service.ts
@@ -1,12 +1,23 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { RestClientV5, WebsocketClient } from 'bybit-api';
-import BaseRestClient from 'bybit-api/lib/util/BaseRestClient';
+import { OHLCVKlineV5, RestClientV5, WebsocketClient } from 'bybit-api';
+import { createObjectCsvWriter } from 'csv-writer';
+import { KlineOutputDto } from './dto/kline.dto';
 
 @Injectable()
 export class HistorianService implements OnModuleInit {
+  private klineOutputList: KlineOutputDto[];
+  private startTimestamp: number;
+  private scanning: boolean;
+
+  constructor() {
+    this.klineOutputList = [];
+    this.startTimestamp = 1685570400000;
+    this.scanning = true;
+  }
+
   onModuleInit() {
     console.log('Historian has been initialized!');
-    this.scanHistoryData().then((r) => console.log('finished'));
+    this.scanHistoryData().then();
   }
 
   async scanHistoryData() {
@@ -14,29 +25,65 @@ export class HistorianService implements OnModuleInit {
       testnet: false,
     });
 
-    client
-      .getKline({
+    let i = 0;
+
+    while (this.scanning && i < 5) {
+      const klineDamp = await client.getKline({
         category: 'spot',
         symbol: 'SOLUSDT',
-        interval: '60',
-        start: 1685570400000,
-        // end: 1685570400000,1685630100000
-      })
-      .then((response) => {
-        this.parseTimestamps(response.result.list);
-      })
-      .catch((error) => {
-        console.error(error);
+        interval: '5',
+        start: this.startTimestamp,
       });
+
+      const timestamps: KlineOutputDto[] = await this.parseKlineData(
+        klineDamp.result.list,
+        i,
+      );
+      this.klineOutputList.push(...timestamps.reverse().slice(0, -1));
+      if (i == 4) {
+        this.klineOutputList.forEach((line) => {
+          console.log(
+            `starttime: ${this.startTimestamp}, time: ${JSON.stringify(line)}`,
+          );
+        });
+      }
+
+      i = i + 1;
+    }
+    // console.log(`klineOutputList: ${this.klineOutputList}`);
   }
 
-  async parseTimestamps(historyArray: any) {
-    const timestamps = historyArray.map((i) => [new Date(Number(i[0])), i]);
-    // timestamps.sort((a, b) => a.getTime() - b.getTime());
-    console.log(timestamps);
-    // console.log(
-    //   `start date: ${timestamps[0]},
-    //   end date: ${timestamps[timestamps.length - 1]}`,
-    // );
+  async parseKlineData(
+    historyList: OHLCVKlineV5[],
+    requestId: number,
+  ): Promise<KlineOutputDto[]> {
+    try {
+      this.startTimestamp = Number(historyList[0][0]);
+      const timestamps: KlineOutputDto[] = historyList.map((list) => {
+        return {
+          requestId: requestId,
+          date: new Date(Number(list[0]))
+            .toLocaleString('en-US', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+              hour12: true,
+            })
+            .replace(/\//g, '-')
+            .replace(',', ''),
+          open: Number(list[1]),
+          high: Number(list[2]),
+          low: Number(list[3]),
+          close: Number(list[4]),
+          volume: Number(list[5]),
+        };
+      });
+      return timestamps;
+    } catch (err) {
+      this.scanning = false;
+      console.error(err);
+    }
   }
 }
